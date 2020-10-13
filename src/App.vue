3ce1f569cbf310fc8760f9814712638ca35590b0
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2020-10-13 18:37:55 -0300

    Added css, fixed logo, removed unused files

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <h1>Attendance Tracker</h1>
-      <h2>Login</h2>
-      <br>
-      <h2> Email Address</h2>
-      <input style='width: 300px' v-model="message" placeholder="Email Address">
-      <h2>Password</h2>
-      <input style='width: 300px' v-model="message" placeholder="Password">
-      <br><br>
-      <button style='width: 310px' >Login</button>
-      <br><br>
-      <button style='width: 310px'  >Create Account</button>
-      <br><br>
-      <button style='width: 310px' >Forgot Password</button>
-      <br><br>
-       <img src="src/assets/UNB.PNG" />
-    </div>
+    <Header />
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from './components/Header'
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 1.1em;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  max-width: 900px;
+  margin: auto;
 }
 </style>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <br>
+    <label for="Email">Email Address</label><br/>
+    <input id="Email" type="text" placeholder="Email Address">
+    <br/><br/>
+    <label for="Password">Password</label><br/>
+    <input id="Password" type="text" placeholder="Password">
+    <br><br>
+    <button>Login</button>
+    <br><br>
+    <button>Create Account</button>
+    <br><br>
+    <button>Forgot Password</button>
+    <br><br>
+
+    <!-- The src must start with a `.` to make it relative to the current directory -->
+    <img src="../assets/UNB.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm'
+}
+</script>
+
+<style scoped lang="scss">
+input, button {
+  width: 300px;
+  height: 30px;
+}
+
+label, button {
+  font-weight: bold;
+}
+
+button {
+  font-size: 16px;
+}
+</style>

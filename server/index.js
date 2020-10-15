cd6fe2d33d0d020ea06151e59c4f154f46b0c689
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2020-10-15 13:00:57 -0300

    Removed example_mongoose and dotenv pluging

--- server/index.js
+++ server/index.js
@@ -3,16 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Load environment variables from .env files
-require('dotenv').config();
-
 // Middleware
 app.use(express.urlencoded()); // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.json()); // Parse JSON bodies (as sent by API clients)
 
-// Define an api route
-const test = require('./routes/api/example_mongoose');
-app.use('/api/test', test);
 
 const server = app.listen(port, () => {
     console.log('App running on port '+port);

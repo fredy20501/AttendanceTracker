7fa9dd9f415351f1f69b3b80100f947d7bdc0f1c
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2020-10-15 21:54:06 -0300

    Fixed jest tests and added new ones

--- jest.config.js
+++ jest.config.js
@@ -24,12 +24,12 @@ module.exports = {
   coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    "/node_modules/"
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
-  // coverageProvider: "babel",
+  coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
--- src/tests/backend.test.js
+++ src/tests/backend.test.js
@@ -1,6 +1,7 @@
-const app = require('../../server/index.js');
+const app = require('../../server/app.js');
+const mongoose = require('mongoose');
 const supertest = require("supertest");
-const request = supertest(app);
+const http = require('http');
 
 describe('Sample Test', () => {
     it('Should test that true === true', () => {
@@ -9,18 +10,73 @@ describe('Sample Test', () => {
 })
 
 describe('Backend server fuctionality', () => {
-    it('reaches server', async done =>{
-        //tests to see if server is reachable
+    let server;
+    let request;
+
+    // Open server before running tests
+    beforeAll((done) => {
+        server = http.createServer(app);
+        server.listen(done);
+        request = supertest(server);
+    });
+
+    // Close database & server when done
+    afterAll(async (done) => {
+        await mongoose.connection.close();
+        server.close(done);
+    });
+
+    it('reaches server api', async done =>{
+        // tests to see if api is reachable
         const response = await request.get("/");
         expect(response.status).toBe(200);
-        //expect(response.body.message).toBe("Hellow, World!");
         done();
-    })
+    });
+
+    it('send invalid login', async done =>{
+        const response = await request.post("/api/login").send({
+            email:'blah', 
+            password:'bbbbbb'
+        });
+        // expect 404 since user should not exist
+        expect(response.status).toBe(404);
+        done();
+    });
+
     it('try to access data without being logged in', async done =>{
-        //sends GET request to /dashboard endpoint
         const response = await request.get("/api/dashboard");
         // expect 401 unauthorized since we are not logged in
         expect(response.status).toBe(401);
         done();
-    })
+    });
+
+    it('create, login, logout, then delete test account', async done =>{
+        // Create test account
+        var response = await request.post("/api/register").send({
+            name:'test_user', 
+            email:'123test@test456.com', 
+            password:'12345',
+            is_professor: false
+        });
+        expect(response.status).toBe(200);
+
+        // Login
+        response = await request.post("/api/login").send({
+            email:'123test@test456.com', 
+            password:'12345'
+        });
+        expect(response.status).toBe(200);
+
+        // Logout
+        response = await request.get("/api/logout");
+        expect(response.status).toBe(200);
+
+        // Delete test account
+        response = await request.delete("/api/delete-user").send({
+            email: '123test@test456.com'
+        });
+        expect(response.status).toBe(200);
+
+        done();
+    });
 })

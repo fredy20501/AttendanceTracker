7efa8d0b123942aab7f88db95ff87f562ea0402d
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2020-10-21 17:04:50 -0300

    Add create-section page, logout button, 404 page  (#9)
    * Add logout button, 404 & create-section pages
    - Also added router logic to prevent unauthorized access to some pages
    
    * Fix typos & add comments

--- src/views/ProfessorHome.vue
+++ src/views/ProfessorHome.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <h1>Welcome professor!</h1>
+    <h1>Professor Home</h1>
+    <div>Welcome {{ getUser.name }}!</div>
+    <br>
+    <button v-on:click="$router.push({ name:'createSection' })">Create Section</button>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'ProfessorHome'
+  name: 'ProfessorHome',
+  computed: {
+    ...mapGetters([
+      'getUser'
+    ])
+  }
 }
 </script>
+
+<style scoped lang="scss">
+button {
+  width: auto;
+  padding: 0 10px;
+}
+</style>

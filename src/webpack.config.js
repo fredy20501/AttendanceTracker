8360e5b0746d2e98122136c1cdaf03248580ac7b
PEKennedy <peter_e_k@outlook.com>
2020-11-08 15:40:29 -0400

    changes to allow frontend-backend connectivity

---
+++ src/webpack.config.js
@@ -0,0 +1,21 @@
+
+/*
+module.exports = {
+
+  devServer:{//disableHostCheck: true,  <<<< not secure, but if all else fails, it might work
+      public: 'https://athena.xn--9xa.network/',
+  },
+};*/
+
+/*
+const path = require('path');
+
+
+
+
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+*/

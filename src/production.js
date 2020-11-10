91dd8412335e4928acf479837b641c6dae59621f
Frederic Verret <43451024+fredy20501@users.noreply.github.com>
2020-11-09 20:49:03 -0400

    Fix typos and add missing code

--- src/production.js
+++ src/production.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const history = require('connect-history-api-fallback')
 const { resolve } = require('path');
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 // Setup the frontend if in production (source: https://dennisreimann.de/articles/vue-cli-serve-express.html)
-if (process.env.NODE_ENV === 'production') {
-  // Since the frontend is only a set of static pages once built we can serve them as static files on a server
-  const publicPath = resolve(__dirname, '../dist')
-  const staticConf = { maxAge: '1y', etag: false }
-  app.use(express.static(publicPath, staticConf))
-  app.use('/', history())
-}
+// Since the frontend is only a set of static pages once built we can serve them as static files on a server
+const publicPath = resolve(__dirname, '../dist')
+const staticConf = { maxAge: '1y', etag: false }
+app.use(express.static(publicPath, staticConf))
+app.use('/', history())
 
 const PORT = 8080
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Frontend server started on port ${PORT}`));

bff4063e1c009908d5c608cf581aa8add4cbedf9
abearcher <thales97@gmail.com>
2020-11-16 16:10:26 -0400

    put getCourseView to section.js

--- server/routes/api/section.js
+++ server/routes/api/section.js
@@ -166,3 +166,44 @@ router.post('/updateSection', (req, res) => {
 });
 
 module.exports = router;
+
+// ------------- Combined Seating Layout and Course Methods -------------
+
+// gets information regarding a course given a student/prof id
+router.get('/getCourseView', (req, res) => {
+    // this is the professor ID
+    let courseID = req.body.courseID;
+
+    Course.findOne( {_id: courseID} , function(err, course){
+        if(err){
+            console.log(err);
+            return res.status(500).send();
+        }
+
+        seatingLayout = SeatingLayout.findOne({_id: course.seating_layout}, function(err, seatingLayout){
+
+            let myLayout;
+            if(err){
+                console.log(err);
+                return res.status(500).send();
+            }
+    
+            if(seatingLayout != null && seatingLayout.layout != null) {
+                myLayout = seatingLayout.layout;
+                console.log("?");
+            } else {
+                myLayout = "Layout not found";
+            }
+
+            return res.status(200).json({
+                "name" : course.name,
+                "alwaysMandatory" : course.always_mandatory,
+                "students": course.students,
+                "seatingLayout" : myLayout
+            });
+        })
+
+    })
+
+
+});

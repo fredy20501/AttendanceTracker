24f2f83578db52f3b2ffbe6ab777a8838a9a8d81
uwera <uwerantag@gmail.com>
2020-11-19 13:43:04 +0200

    Added tests for endpoints defined in section.js and professor.js

--- tests/backend.test.js
+++ tests/backend.test.js
@@ -97,24 +97,19 @@ describe('Backend server fuctionality', () => {
         });
         const prof1 = response.body.user
 
-        response = await request.delete("/api/section/deleteSeatingLayout").send({
-            name: 'testLayout' 
-        });
-
         response = await request.post("/api/section/createSeatingLayout").send({
             name: 'testLayout',
             capacity : 1,
             dimension: [1,1],
             layout:[[0]],
-            default: false,
+            default: true,
             description: 'test',
             createdBy: prof1._id
         });
+
         expect(response.status).toBe(200);
+
         await request.get("/api/logout");
-        response = await request.delete("/api/section/deleteSeatingLayout").send({
-            name: 'testLayout' 
-        });
         done()
     });
     /* No update-seating-plan endpoint
@@ -316,10 +311,135 @@ describe('Backend server fuctionality', () => {
         response = await request.delete("/api/section/deleteSection").send({
             name: 'testSection' 
         });
-        //expect(response.status).toBe(200);
+        expect(response.status).toBe(200);
         await request.get("/api/logout");
         done();
     });
 
+    it("Should reach pushNewAttendance endpoint", async done => {
+        var response = await request.post("/api/login").send({
+            email:'test.professor@unb.ca', 
+            password:'testing123'
+        });
+        //delete users and section in case they already exist in the database
+        response = await request.delete("/api/delete-user").send({
+            email: 'st1@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'st2@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'prof1@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'admin@test.com'
+        });
+        response = await request.delete("/api/section/deleteSection").send({
+            name: 'testSection'
+        });
+
+        response = await request.post('/api/register').send({
+            email: 'st1@test.com',
+            name: 'Student 1',
+            password:'st1234',
+            is_professor: false
+        });
+        const student1 = response.body.user
+
+        response = await request.post('/api/register').send({
+            email: 'st2@test.com',
+            name: 'Student 2',
+            password:'st2345',
+            is_professor: false
+        });
+        const student2 = response.body.user
+
+        response = await request.post('/api/register').send({
+            email: 'prof1@test.com',
+            name: 'A Professor',
+            password:'pr1234of',
+            is_professor: true
+        });
+        const prof1 = response.body.professor;
+
+        response = await request.post('/api/register').send({
+            email: 'admin@test.com',
+            name: 'An Admin',
+            password:'ad1234min',
+            is_professor: true
+        });
+        const admin1 = response.body.user
+
+        // Create a sample seating layout for this test
+        response = await request.post('/api/section/createSeatingLayout').send({
+            name: 'testLayout',
+            capacity: 4,
+            dimensions: [ 2 , 2],
+            layout: [
+                [1, 1],
+                [1, 1]
+            ], 
+            default: true,
+            description: 'This is a sample',
+            createdBy: prof1
+        });
+        const layout1 = response.body.seatingLayout
+
+        //Create test course
+        response = await request.post('/api/section/createSection').send({
+            courseName: 'testSection',
+            attendanceThreshold: '0',
+            seatingLayout: layout1._id,
+            attMandatory: false,
+            professor: prof1._id,
+            admin: admin1._id,
+            students: [student1, student2],
+            maxCapacity: 30,
+            seatingArrangement: [
+                [null, student1, null, null, null],
+            ],
+            classList: [],
+            attendance: [Date.now(), student1, false]
+        });
+        const course1 = response.body.newSection
+
+        response = await request.put("/api/professor/pushNewAttendance").send({
+            courseID: testSection._id,
+            absent_students: [student1],
+            mandatory: false
+        });
+        expect(response.status).toBe(200);
+        await request.get("/api/logout");
+
+        response = await request.delete("/api/delete-user").send({
+            email: 'st1@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'st2@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'prof1@test.com'
+        });
+        response = await request.delete("/api/delete-user").send({
+            email: 'admin@test.com'
+        });
+        response = await request.delete("/api/section/deleteSection").send({
+            name: 'testSection'
+        });
+        expect(response.status).toBe(200);
+
+        done();
+
+    });
+
+    xit("Should reach getCourseView endpoint", async done => {
+        var response = await request.post("/api/login").send({
+            email:'test.professor@unb.ca', 
+            password:'testing123'
+        });
+        response = await request.get("/api/section/getCourseView");
+        expect(response.status).toBe(200);
+        await request.get("/api/logout");
+    });
 
 })
--- tests/database.test.js
+++ tests/database.test.js
@@ -26,30 +26,30 @@ describe('Database Functionality', () => {
 
     it('Should store user to database', async done => {
         var response = await request.delete("/api/delete-user").send({
-            email: 'dummy@test.com'
+            email: 'testUser@test.com'
         });
         //Send request to register a user
         var user = await request.post('/api/register').send({
-            email: 'dummy@test.com',
-            name: 'dummy test',
+            email: 'testUser@test.com',
+            name: 'test user',
             password:'12345',
             is_professor: true
         });
 
         //Search user in database by email
         user = await User.findOne({
-            email: 'dummy@test.com'
+            email: 'testUser@test.com'
         });
 
         //Check that user information stored in the database is correct
-        expect(user.name).toBe('dummy test');
-        expect(user.email).toBe('dummy@test.com')
+        expect(user.name).toBe('test user');
+        expect(user.email).toBe('testUser@test.com')
         expect(user.password).toBe('12345')
         expect(user.is_professor).toBe(true);
 
         //Delete test user after testing
         response = await request.delete("/api/delete-user").send({
-            email: 'dummy@test.com'
+            email: 'testUser@test.com'
         });
         expect(response.status).toBe(200);
 
@@ -71,7 +71,7 @@ describe('Database Functionality', () => {
             email: 'admin@test.com'
         });
         response = await request.delete("/api/section/deleteSeatingLayout").send({
-            name: 'Sample layout'
+            name: 'testLayout'
         });
         response = await request.delete("/api/section/deleteSection").send({
             name: 'testSection' 
@@ -81,6 +81,7 @@ describe('Database Functionality', () => {
             email:'test.professor@unb.ca', 
             password:'testing123'
         });
+
         //Create 2 students, an admin, and a professor
         response = await request.post('/api/register').send({
             email: 'st1@test.com',
@@ -139,12 +140,12 @@ describe('Database Functionality', () => {
         response = await request.post('/api/section/createSection').send({
             courseName: 'testSection',
             attendanceThreshold: '0',
-            seatingLayout: layout1._id,
+            seatingLayout: layout1,
             attMandatory: false,
             professor: prof1,
             admin: admin1,
             students: [student1, student2],
-            maxCapacity: 30,
+            maxCapacity: 25,
             seatingArrangement: [
                 [null, student1, null, null, null],
             ],

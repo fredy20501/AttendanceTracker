43386d69ba5f73a03db1a851d0e010bfbf59df02
Nathanael Mack <nmack1500@gmail.com>
2020-11-23 11:27:20 -0400

    Added greeting message that distingishes between user types and displays the user's name.

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -2,11 +2,27 @@
 <template>
   <header class="banner">
     <h1>Attendance Tracker</h1>
+    <div v-if="isAuthenticated">
+      <div v-if="isProfessor">
+        <h2>Welcome, Professor {{this.getUser.name}}</h2>
+      </div>
+      <div v-else>
+        <h2>Welcome, {{this.getUser.name}}</h2>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "Header"
+  name: "Header",
+
+  computed: {
+    // Import the getters from the global store
+    ...mapGetters(["getUser", "isAuthenticated","isProfessor"]),
+  }
 }
+
 </script>

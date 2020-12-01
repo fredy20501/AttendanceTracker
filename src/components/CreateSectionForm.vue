e399a16b9acea2530e948fcedc846e9ae03b2053
TashZaman <58598307+TashZaman@users.noreply.github.com>
2020-12-01 08:10:39 -0400

    Changing "Whiteboard" to "Front of Classroom" (#56)

--- src/components/CreateSectionForm.vue
+++ src/components/CreateSectionForm.vue
@@ -195,7 +195,7 @@
                 <td></td>
                 <td :colspan="currentLayout.layout[0].length">
                   <div style="margin: 10px auto 0 auto; padding: 0 10px; max-width: 200px;" class="border">
-                    Whiteboard
+                    Front of Classroom
                   </div>
                 </td>
               </tr>
